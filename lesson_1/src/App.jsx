296193cdc4c66abe6564cce46f8d2671087ff7e5
Yevhenii <biloys1337@gmail.com>
2021-02-21 18:04:58 +0200

    init lesson 1

---
+++ lesson_1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>hello guys</h1>
+      </div>
+    );
+  }
+}
